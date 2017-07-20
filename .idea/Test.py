4a90e9385dfa4c8703878c87a1d589ac7008168e
pupman316 <pupman316@gmail.com>
2017-07-19 20:41:07 -0500

    Modified from home @ 8:40 PM on 7-19-17

--- .idea/Test.py
+++ .idea/Test.py
@@ -4,12 +4,14 @@ import numpy as np
 print("Hello Python!\n\n")
 
 t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
+v = 1 + np.sin(2*np.pi*t)
+plt.plot(t, v)
 
 plt.xlabel('time (s)')
 plt.ylabel('voltage (mV)')
 plt.title('As simple as it gets, folks')
 plt.grid(True)
-plt.savefig("test.png")
+# plt.savefig("test.png") # Just saving the figure as a picture in the same directory
 plt.show()
+
+print("Goodbye Python.")
